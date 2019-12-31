8f45e1ac545d1dff41c2e55cf909dbae9c6e82fc
petermaringer <petermaringer@notmailinator.com>
2019-12-31 22:52:19 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UITe
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         lb.text = lb.text! + "\(textField)"
-        //print("TextField did end editing method called")
+        lb.frame.size.width = self.view.frame.width
     }
     
     
