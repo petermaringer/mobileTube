0bc92a245c41de2a92c91a0a02a6e5e681548d7e
petermaringer <petermaringer@notmailinator.com>
2020-01-05 12:00:07 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UITe
   var lb: UILabel!
   
   var tableView: UITableView!
-  let array: NSArray = ["https://google.com","https://orf.at","https://derstandard.at","https://welt.de","https://willhaben.at","https://www.aktienfahrplan.com"]
+  let array: NSArray = ["https://google.com","https://orf.at","https://derstandard.at","https://welt.de","https://willhaben.at","https://www.aktienfahrplan.com/plugins/rippletrading/rippletest.cgi"]
   
   var url: URL!
   var defaultUserAgent: String = "default"
