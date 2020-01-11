cac5c880dab1e6bcac5e6a6461e9d64cbe0f2d23
petermaringer <petermaringer@notmailinator.com>
2020-01-11 11:53:09 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -578,7 +578,8 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UITe
     var bflist = "bflist:"
     let historySize = webview.backForwardList.backList.count
     for index in 1...historySize {
-      bflist = bflist + " " + webview.backForwardList.item(at: -index)!.url.absoluteString
+      //bflist = bflist + " " + webview.backForwardList.item(at: -index)!.url.absoluteString
+      bflist = bflist + " \(index)/\(historySize)"
     }
     
     //var bflist = "bflist:"
