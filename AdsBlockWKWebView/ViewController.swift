74235c29b8f9d6ed08a2afcb6dc5bab9bfe2bdc7
petermaringer <petermaringer@notmailinator.com>
2020-01-18 08:53:31 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -170,6 +170,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UITe
     urlField.endEditing(true)
     
     playerViewController().playVideo()
+    playerViewController.player = nil
     
     let delegate = UIApplication.shared.delegate as! AppDelegate
     let deviceToken = delegate.sesscat
