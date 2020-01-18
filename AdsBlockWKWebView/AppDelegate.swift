ba548400b42c1db606e17001dc075ac00e6eb77d
petermaringer <petermaringer@notmailinator.com>
2020-01-18 10:42:56 +0100

    bu

--- AdsBlockWKWebView/AppDelegate.swift
+++ AdsBlockWKWebView/AppDelegate.swift
@@ -53,10 +53,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        playerViewController.player = nil
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        playerViewController.player = player
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
