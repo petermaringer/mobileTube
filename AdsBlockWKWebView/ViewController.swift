324f71b68537f0ddb0de89b7fd4c5740e61e204b
petermaringer <petermaringer@notmailinator.com>
2020-01-20 16:15:02 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -223,7 +223,7 @@ player.play()*/
   //var avPVC: AVPlayerViewController!
   //avPVC = AVPlayerViewController()
   
-  if let targetSC = UIApplication.shared.windows[2].rootViewController!.childViewControllers.first(where: { $0 is AVPlayerViewController }) as? AVPlayerViewController
+  if let targetSC = UIApplication.shared.windows[2].rootViewController!.childViewControllers.first(where: { $0 is AVPlayerViewController }) as? AVPlayerViewController {
   //if targetSC != nil {
   //avPVC = targetSC!
   lb.text = lb.text! + " VC:\(targetSC)"
