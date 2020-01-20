eb94c08291da3587a6449705f7356ebaa5de2198
petermaringer <petermaringer@notmailinator.com>
2020-01-20 14:39:47 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -216,13 +216,13 @@ player.play()*/
   var avPVC: AVPlayerViewController!
   avPVC = AVPlayerViewController()
   
-  let targetSC = UIApplication.shared.windows[2].rootViewController!.childViewControllers.first(where: { $0 is AVPlayerViewController }) as? AVPlayerViewController
+  if let targetSC = UIApplication.shared.windows[2].rootViewController!.childViewControllers.first(where: { $0 is AVPlayerViewController }) as? AVPlayerViewController {
   //if targetSC != nil {
   //avPVC = targetSC!
-  lb.text = lb.text! + " VC:\(targetSC!)"
-  lb.text = lb.text! + " VCP:\(targetSC!.player!)"
+  lb.text = lb.text! + " VC:\(targetSC)"
+  lb.text = lb.text! + " VCP:\(targetSC.player!)"
   adjustLabel()
-  //}
+  }
     
     /*var viewlist = "list:"
     func findViewWithAVPlayerLayer(view: UIView) -> UIView? {
