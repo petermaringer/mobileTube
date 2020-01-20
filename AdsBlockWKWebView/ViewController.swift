496e07f0ae74525cb7adaf1a5f6ed00e10f23a8a
petermaringer <petermaringer@notmailinator.com>
2020-01-20 16:03:03 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -223,13 +223,13 @@ player.play()*/
   //var avPVC: AVPlayerViewController!
   //avPVC = AVPlayerViewController()
   
-  let targetSC = UIApplication.shared.windows[2].rootViewController!.childViewControllers.first(where: { $0 is AVPlayerViewController })
-  //if targetSC != nil {
+  let targetSC = UIApplication.shared.windows[2].rootViewController!.childViewControllers.first(where: { $0 is AVPlayerViewController }) as? AVPlayerViewController
+  if targetSC != nil {
   //avPVC = targetSC!
   lb.text = lb.text! + " VC:\(targetSC)"
   //lb.text = lb.text! + " VCP:\(targetSC!.player!)"
   adjustLabel()
-  //}
+  }
     
     /*var viewlist = "list:"
     func findViewWithAVPlayerLayer(view: UIView) -> UIView? {
