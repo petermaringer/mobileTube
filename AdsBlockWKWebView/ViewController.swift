ae1e67e216256d4e629c3487a47e0032e3cda995
petermaringer <petermaringer@notmailinator.com>
2020-10-13 20:25:22 +0200

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -876,7 +876,9 @@ player.play()*/
         self.webview.reload()
       }
     } else {
-      webview.customUserAgent = nil
+      webview.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1"
+      //webview.customUserAgent = nil
+      
       //webview.customUserAgent = defaultUserAgent
       //defaultUserAgent = "default"
       webview.reload()
