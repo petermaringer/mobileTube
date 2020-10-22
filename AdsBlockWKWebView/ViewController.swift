840bb0bac0a9b6b34cfd5be698af0d7892a9bd54
petermaringer <petermaringer@notmailinator.com>
2020-10-22 11:41:50 +0200

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -458,8 +458,11 @@ player.play()*/
   }
   
   @objc func editButtonClicked(url: String) {
-    //webview.go(to: webview.backForwardList.item(at: webview.backForwardList.forwardList.count * -1 - 2)!)
-    webview.go(to: webview.backForwardList.item(at: -2)!)
+    
+    if webview.backForwardList.forwardList.count == 0 {
+      webview.go(to: webview.backForwardList.item(at: -2)!)
+    }
+    
     showAlert(message: "E:\(url)")
     //lb.text = lb.text! + " E"
     //adjustLabel()
