64296f6e0030fe527e2b9b4680d3fb2e9ea66624
petermaringer <petermaringer@notmailinator.com>
2020-10-24 01:45:52 +0200

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -665,8 +665,8 @@ player.play()*/
       webview.frame.size.width = self.view.frame.width - insetL - insetR
       webview.frame.size.height = self.view.frame.height - insetT - insetB - urlField.frame.size.height - 10
       
-      //webview.frame.origin.y = 0
-      //webview.frame.size.height = self.view.frame.height
+      webview.frame.origin.y = 0
+      webview.frame.size.height = self.view.frame.height
       
       if webview2.isDescendant(of: view) {
         webview.frame.origin.y += 200
@@ -681,7 +681,7 @@ player.play()*/
       //webview.scrollView.frame.origin.y = insetT + urlField.frame.size.height + 10
       //webview.scrollView.frame.size.height = self.view.frame.height - insetT - urlField.frame.size.height - 10
       
-      /*
+      
       webview.setValue(true, forKey: "_haveSetObscuredInsets")
       webview.setValue(UIEdgeInsets(top: insetT + urlField.frame.size.height + 10, left: 0, bottom: insetB, right: 0), forKey: "_obscuredInsets")
       webview.scrollView.contentInset = UIEdgeInsets(top: insetT + urlField.frame.size.height + 10, left: 0, bottom: insetB, right: 0)
@@ -690,7 +690,7 @@ player.play()*/
       }
       //webview.scrollView.scrollIndicatorInsets = webview.scrollView.contentInset
       webview.scrollView.scrollIndicatorInsets = UIEdgeInsets(top: urlField.frame.size.height + 10, left: 0, bottom: 0, right: 0)
-      */
+      
       
       webview3.frame.origin.x = insetL
       //webview3.frame.origin.y = insetT + 5
