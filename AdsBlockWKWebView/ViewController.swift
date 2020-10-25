26a60987eec25a2b318568cb29f29f14d6cbaa1b
petermaringer <petermaringer@notmailinator.com>
2020-10-25 10:59:24 +0100

    bu

--- AdsBlockWKWebView/ViewController.swift
+++ AdsBlockWKWebView/ViewController.swift
@@ -790,10 +790,9 @@ webviewConfig.userContentController.addUserScript(WKUserScript(source: "var el =
         
         //topNavBgView = UIView(frame: CGRect.zero)
         //topNavBgView.backgroundColor = UIColor.viewBgColor.withAlphaComponent(0.95)
-        //topNavBgView = UIVisualEffectView(frame: CGRect.zero)
-        topNavBgView.frame = CGRect.zero
+        topNavBgView = UIVisualEffectView(frame: CGRect.zero)
         topNavBgView.effect = UIBlurEffect(style: UIBlurEffect.Style.regular)
-        //topNavBgView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        topNavBgView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(topNavBgView)
         
         urlField = UITextField(frame: CGRect.zero)
